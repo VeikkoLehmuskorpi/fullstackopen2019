988642f4a44b161203c5b727affb9c2e12af8e7a
VeikkoLehmuskorpi <veikko.lehmuskorpi@student.lamk.fi>
2019-07-05 14:25:17 +0300

    osa4a: Run tests on the blog posts

--- osa4/utils/blogListHelper.js
+++ osa4/utils/blogListHelper.js
@@ -1,5 +1,19 @@
 const dummy = () => 1;
 
+const totalLikes = blogs => {
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+  // zero likes if no blogs supplied
+  if (blogs.length === 0) {
+    return 0;
+  }
+
+  // get an array of just the likes and reduce it
+  const blogLikes = blogs.map(blog => blog.likes);
+  return blogLikes.reduce(reducer, 0);
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
