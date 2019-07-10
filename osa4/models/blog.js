43f41a9cf619b3ef76b21cc116f5e39c6e60f39c
VeikkoLehmuskorpi <veikko.lehmuskorpi@student.lamk.fi>
2019-07-10 20:48:07 +0300

    Add a toJSON method to modify the blog object returned from MongoDB

--- osa4/models/blog.js
+++ osa4/models/blog.js
@@ -7,6 +7,18 @@ const blogSchema = mongoose.Schema({
   likes: Number,
 });
 
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-param-reassign */
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+/* eslint-enable no-param-reassign */
+/* eslint-enable no-underscore-dangle */
+
 mongoose.set('useFindAndModify', false);
 
 module.exports = mongoose.model('Blog', blogSchema);
