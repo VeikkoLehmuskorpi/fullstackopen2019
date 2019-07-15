1994bc28a2d74aa234766e8887505d3cb104cf13
VeikkoLehmuskorpi <veikko.lehmuskorpi@student.lamk.fi>
2019-07-15 10:29:47 +0300

    Implement users router in the app

--- osa4/app.js
+++ osa4/app.js
@@ -7,6 +7,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const config = require('./utils/config');
 const blogsRouter = require('./controllers/blogs');
+const usersRouter = require('./controllers/users');
 
 console.log(`Connecting to ${config.DB_URI} ...`);
 
@@ -19,5 +20,6 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/api/blogs', blogsRouter);
+app.use('/api/users', usersRouter);
 
 module.exports = app;
