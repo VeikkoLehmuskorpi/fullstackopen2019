6d1f5d5aee80aaa8253409fa8896e6afc9ea5b18
VeikkoLehmuskorpi <veikko.lehmuskorpi@student.lamk.fi>
2019-07-15 17:22:19 +0300

    Implement login controller in the app

--- osa4/app.js
+++ osa4/app.js
@@ -8,6 +8,7 @@ const mongoose = require('mongoose');
 const config = require('./utils/config');
 const blogsRouter = require('./controllers/blogs');
 const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
 
 const middleware = require('./utils/middleware');
 
@@ -23,6 +24,7 @@ app.use(bodyParser.json());
 
 app.use('/api/blogs', blogsRouter);
 app.use('/api/users', usersRouter);
+app.use('/api/login', loginRouter);
 
 app.use(middleware.errorHandler);
 
