4583f75de99b398dacc9f419319eceaddbab14c4
VeikkoLehmuskorpi <veikko.lehmuskorpi@student.lamk.fi>
2019-07-15 17:06:04 +0300

    Add user to the blog schema

--- osa4/models/blog.js
+++ osa4/models/blog.js
@@ -5,6 +5,10 @@ const blogSchema = mongoose.Schema({
   author: String,
   url: String,
   likes: { type: Number, default: 0 },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
 });
 
 /* eslint-disable no-underscore-dangle */
