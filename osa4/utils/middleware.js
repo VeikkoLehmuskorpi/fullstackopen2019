5757e7959275b1790ced3fb68e877f4173ec62c8
VeikkoLehmuskorpi <veikko.lehmuskorpi@student.lamk.fi>
2019-07-15 17:34:29 +0300

    Add cast, objectid and jwt errors to the middleware

--- osa4/utils/middleware.js
+++ osa4/utils/middleware.js
@@ -2,8 +2,14 @@
 const errorHandler = (error, request, response, next) => {
   console.error(error.message);
 
+  if (error.name === 'CastError' && error.kind === 'ObjectId') {
+    return response.status(400).json({ error: 'malformatted id' });
+  }
   if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message });
+  }
+  if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({ error: 'invalid token' });
   }
 
   return next(error);
