85cb532ca43b4dce07be89ddf51f93059efe8e4a
VeikkoLehmuskorpi <veikko.lehmuskorpi@student.lamk.fi>
2019-07-15 11:00:23 +0300

    Handle users get and post requests

--- osa4/controllers/users.js
+++ osa4/controllers/users.js
@@ -1,9 +1,35 @@
 const usersRouter = require('express').Router();
-// const User = require('../models/user');
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
 
 // GET
 usersRouter.get('/', async (request, response) => {
-  response.send('todo list users here');
+  const users = await User.find({});
+  const usersFormatted = await users.map(user => user.toJSON());
+
+  response.json(usersFormatted);
+});
+
+// POST
+usersRouter.post('/', async (request, response, next) => {
+  try {
+    const { body } = request;
+
+    const saltRounds = 10;
+    const passwordHash = await bcrypt.hash(body.password, saltRounds);
+
+    const user = new User({
+      username: body.username,
+      name: body.name,
+      passwordHash,
+    });
+
+    const savedUser = await user.save();
+
+    response.json(savedUser);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = usersRouter;
