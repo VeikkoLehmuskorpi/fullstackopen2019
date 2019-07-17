7a89e4cbeb6a94bac1af7b7f541576e617d849f9
VeikkoLehmuskorpi <veikko.lehmuskorpi@student.lamk.fi>
2019-07-17 13:05:55 +0300

    Use example bloglist backend and separate front and back

---
+++ osa5/bloglist-backend/models/user.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    minlength: 3,
+    present: true,
+  },
+  name: String,
+  passwordHash: String,
+  blogs: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Blog',
+    },
+  ],
+});
+
+userSchema.plugin(uniqueValidator);
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    delete returnedObject.passwordHash;
+  },
+});
+
+mongoose.set('useCreateIndex', true);
+mongoose.set('useFindAndModify', false);
+
+const User = mongoose.model('BlogUser', userSchema);
+
+module.exports = User;
