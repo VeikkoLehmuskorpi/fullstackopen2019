2c5c9c09b046e7dd474a1e8186aeaaa45fddf1c0
VeikkoLehmuskorpi <veikkolehmuskorpi@gmail.com>
2019-07-23 20:27:37 +0300

    Pass user token from BlogList to Blog

--- osa5/bloglist-frontend/src/components/BlogList.js
+++ osa5/bloglist-frontend/src/components/BlogList.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Blog from './Blog';
 
-const BlogList = ({ blogs }) => {
-  return blogs.map(blog => <Blog key={blog.id} blog={blog} />);
+const BlogList = ({ blogs, token }) => {
+  return blogs.map(blog => <Blog key={blog.id} blog={blog} token={token} />);
 };
 
 export default BlogList;
