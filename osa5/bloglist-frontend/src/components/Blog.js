b2a5c074e321f31820f52b6909c1e1c65d57b215
VeikkoLehmuskorpi <veikkolehmuskorpi@gmail.com>
2019-07-23 19:37:05 +0300

    Style individual Blog component

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+const Blog = ({ blog }) => {
+  const [detailsVisible, setDetailsVisible] = useState(false);
+
+  const toggleDetailsVisibility = () => setDetailsVisible(!detailsVisible);
+
+  const blogStyle = {
+    background: '#f4f4f4',
+    padding: '1rem',
+    margin: '0 0 1rem 0',
+  };
+
+  if (detailsVisible) {
+    return (
+      <div style={blogStyle}>
+        <div onClick={toggleDetailsVisibility}>
+          {blog.title} {blog.author}
+        </div>
+        <div>{blog.url}</div>
+        <div>
+          {blog.likes} likes <button>like</button>
+        </div>
+        <div>added by {blog.user.name}</div>
+      </div>
+    );
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div onClick={toggleDetailsVisibility}>
+        {blog.title} {blog.author}
+      </div>
+    </div>
+  );
+};
 
 export default Blog;
