5a2cd8c2aa664b783072e929a363518c812b257c
VeikkoLehmuskorpi <veikkolehmuskorpi@gmail.com>
2019-07-23 20:28:01 +0300

    Handle like button inside Blog

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
+import blogService from '../services/blogs';
 
-const Blog = ({ blog }) => {
+const Blog = ({ blog, token }) => {
   const [detailsVisible, setDetailsVisible] = useState(false);
 
   const toggleDetailsVisibility = () => setDetailsVisible(!detailsVisible);
@@ -9,6 +10,15 @@ const Blog = ({ blog }) => {
     background: '#f4f4f4',
     padding: '1rem',
     margin: '0 0 1rem 0',
+  };
+
+  const handleblogLike = async () => {
+    const updatedFields = {
+      likes: blog.likes + 1,
+    };
+
+    const modifiedBlog = await blogService.update(blog, updatedFields, token);
+    console.log(modifiedBlog);
   };
 
   return (
@@ -20,7 +30,7 @@ const Blog = ({ blog }) => {
         <>
           <div>{blog.url}</div>
           <div>
-            {blog.likes} likes <button>like</button>
+            {blog.likes} likes <button onClick={handleblogLike}>like</button>
           </div>
           <div>added by {blog.user.name}</div>
         </>
