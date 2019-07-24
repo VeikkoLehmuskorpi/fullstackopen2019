8165650727d7f987cfeb9874ff503637c34ad81f
VeikkoLehmuskorpi <veikkolehmuskorpi@gmail.com>
2019-07-24 22:51:36 +0300

    Spread props from BlogList to Blog

--- osa5/bloglist-frontend/src/components/BlogList.js
+++ osa5/bloglist-frontend/src/components/BlogList.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import Blog from './Blog';
 
-const BlogList = ({ blogs, user, handleblogLike, handleBlogRemove }) => {
+const BlogList = ({ blogs, ...props }) => {
   return blogs
     .sort((a, b) => b.likes - a.likes)
-    .map(blog => (
-      <Blog
-        key={blog.id}
-        blog={blog}
-        user={user}
-        handleblogLike={handleblogLike}
-        handleBlogRemove={handleBlogRemove}
-      />
-    ));
+    .map(blog => <Blog key={blog.id} blog={blog} {...props} />);
 };
 
 export default BlogList;
