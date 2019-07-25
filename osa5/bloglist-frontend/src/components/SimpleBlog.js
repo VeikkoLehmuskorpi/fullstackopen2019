a3240f936b24524129a615a68b7cbdd8ee886f0c
VeikkoLehmuskorpi <veikkolehmuskorpi@gmail.com>
2019-07-25 18:19:20 +0300

    Run tests on SimpleBlog

--- osa5/bloglist-frontend/src/components/SimpleBlog.js
+++ osa5/bloglist-frontend/src/components/SimpleBlog.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 const SimpleBlog = ({ blog, onClick }) => (
   <div>
-    <div className='titleAndAuthor'>
+    <div className='title-author'>
       {blog.title} {blog.author}
     </div>
     <div className='likes'>
       blog has {blog.likes} likes
-      <button onClick={onClick}>like</button>
+      <button className='like-button' onClick={onClick}>
+        like
+      </button>
     </div>
   </div>
 );
--- osa5/bloglist-frontend/src/components/SimpleBlog.test.js
+++ osa5/bloglist-frontend/src/components/SimpleBlog.test.js
@@ -1,33 +1,47 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import SimpleBlog from './SimpleBlog';
 
 describe('<SimpleBlog/>', () => {
-  let component;
-
-  let blog = {
-    title: 'Testing React components',
-    author: 'Veikko Lehmuskorpi',
-    likes: 3,
-  };
-
-  beforeEach(() => {
-    component = render(
-      <SimpleBlog
-        blog={blog}
-        onClick={() => (blog.likes = blog.likes + 1)}
-      ></SimpleBlog>
-    );
-  });
-
   test('renders title, author and likes', () => {
+    const blog = {
+      title: 'Testing React components',
+      author: 'Veikko Lehmuskorpi',
+      likes: 3,
+    };
+
+    const onClick = jest.fn();
+
+    const component = render(
+      <SimpleBlog blog={blog} onClick={onClick}></SimpleBlog>
+    );
+
     component.debug();
 
-    const titleAndAuthor = component.container.querySelector('.titleAndAuthor');
+    const titleAndAuthor = component.container.querySelector('.title-author');
     expect(titleAndAuthor).toHaveTextContent('Testing React components');
     expect(titleAndAuthor).toHaveTextContent('Veikko Lehmuskorpi');
 
     const likes = component.container.querySelector('.likes');
     expect(likes).toHaveTextContent('blog has 3 likes');
+  });
+
+  test('like button event handler gets called every click', () => {
+    const blog = {
+      title: 'Testing React components',
+      author: 'Veikko Lehmuskorpi',
+      likes: 3,
+    };
+
+    const onClick = jest.fn();
+
+    const { getByText } = render(
+      <SimpleBlog blog={blog} onClick={onClick}></SimpleBlog>
+    );
+
+    const likeButton = getByText('like');
+    fireEvent.click(likeButton);
+
+    expect(onClick).toHaveBeenCalled();
   });
 });
