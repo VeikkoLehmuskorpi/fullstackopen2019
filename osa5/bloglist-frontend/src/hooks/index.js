320a24f4a21846bfffa29dc6280cdb29c83f9c1d
VeikkoLehmuskorpi <veikkolehmuskorpi@gmail.com>
2019-07-28 14:58:19 +0300

    Create useResource hook

--- osa5/bloglist-frontend/src/hooks/index.js
+++ osa5/bloglist-frontend/src/hooks/index.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import axios from 'axios';
 
 // useField
 export const useField = type => {
@@ -14,4 +15,65 @@ export const useField = type => {
     onChange,
     reset,
   };
+};
+
+export const useResource = endpoint => {
+  const [resources, setResource] = useState([]);
+
+  const headers = token => {
+    return {
+      headers: {
+        Authorization: `bearer ${token}`,
+      },
+    };
+  };
+
+  const resourceService = {
+    getAll: async () => {
+      const response = await axios.get(endpoint);
+
+      setResource([...resources, ...response.data]);
+    },
+    create: async (obj, token) => {
+      const response = await axios.post(endpoint, obj, headers(token));
+
+      setResource([...resources, response.data]);
+    },
+    update: async (obj, updatedFields, token) => {
+      const updatedObj = {
+        ...obj,
+        ...updatedFields,
+      };
+
+      console.log('obj', obj);
+      console.log('updatedFields', updatedFields);
+      console.log('updatedObj', updatedObj);
+
+      const response = await axios.put(
+        `${endpoint}/${obj.id}`,
+        updatedObj,
+        headers(token)
+      );
+
+      const updatedResource = response.data;
+
+      updatedResource.user = {
+        id: obj.user.id,
+        name: obj.user.name,
+        username: obj.user.username,
+      };
+
+      setResource([
+        ...resources.filter(resource => resource.id !== updatedResource.id),
+        updatedResource,
+      ]);
+    },
+    remove: async ({ id }, token) => {
+      await axios.delete(`${endpoint}/${id}`, headers(token));
+
+      setResource(resources.filter(resource => resource.id !== id));
+    },
+  };
+
+  return [resources, resourceService];
 };
