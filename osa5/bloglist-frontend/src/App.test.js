1f35d6c6efd9c50ccacd868802aa57346038ad6c
VeikkoLehmuskorpi <veikkolehmuskorpi@gmail.com>
2019-07-28 11:12:53 +0300

    Test rendering of blogs in App

--- osa5/bloglist-frontend/src/App.test.js
+++ osa5/bloglist-frontend/src/App.test.js
@@ -3,18 +3,17 @@ import { render, waitForElement } from '@testing-library/react';
 jest.mock('./services/blogs');
 import App from './App';
 
-beforeEach(() => {
-  const user = {
-    username: 'tester',
-    token: '1231231214',
-    name: 'Donald Tester',
-  };
-
-  localStorage.setItem('loggedInBloglistUser', JSON.stringify(user));
-});
-
 describe('<App />', () => {
   test('renders all blogs it gets from backend', async () => {
+    // login
+    const user = {
+      username: 'tester',
+      token: '1231231214',
+      name: 'Donald Tester',
+    };
+
+    localStorage.setItem('loggedInBloglistUser', JSON.stringify(user));
+
     const component = render(<App />);
     component.rerender(<App />);
     await waitForElement(() => component.container.querySelector('.blog'));
@@ -29,5 +28,17 @@ describe('<App />', () => {
     expect(component.container).toHaveTextContent(
       'The most important methods of HTTP are GET and POST'
     );
+  });
+
+  test('doesnt render blogs without a logged in user', async () => {
+    // clear localstorage user
+    localStorage.setItem('loggedInBloglistUser', null);
+
+    const component = render(<App />);
+
+    await waitForElement(() => component.getByText('Login'));
+
+    const blogs = component.container.querySelectorAll('.blog');
+    expect(blogs.length).toBe(0);
   });
 });
