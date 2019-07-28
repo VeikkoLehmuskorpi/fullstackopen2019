a3ef8ad4a1d8d9f13391e8ec72ba9528e486f0e0
VeikkoLehmuskorpi <veikkolehmuskorpi@gmail.com>
2019-07-28 11:03:56 +0300

    Add mock localstorage api in jest setup

--- osa5/bloglist-frontend/src/setupTests.js
+++ osa5/bloglist-frontend/src/setupTests.js
@@ -5,3 +5,31 @@ import '@testing-library/jest-dom/extend-expect';
 
 // this is basically: afterEach(cleanup)
 import '@testing-library/react/cleanup-after-each';
+
+// mock localstorage api
+let savedItems = {};
+
+const localStorageMock = {
+  setItem: (key, item) => {
+    savedItems[key] = item;
+  },
+  getItem: key => savedItems[key],
+  clear: (savedItems = {}),
+};
+
+Object.defineProperty(window, 'localStorage', { value: localStorageMock });
+
+// supress unnecessary act() warning
+const originalError = console.error;
+beforeAll(() => {
+  console.error = (...args) => {
+    if (/Warning.*not wrapped in act/.test(args[0])) {
+      return;
+    }
+    originalError.call(console, ...args);
+  };
+});
+
+afterAll(() => {
+  console.error = originalError;
+});
