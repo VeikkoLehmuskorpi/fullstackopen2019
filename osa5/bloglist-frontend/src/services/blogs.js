efd27e79ed50a75012697aa66c36b68a15ec1a43
VeikkoLehmuskorpi <veikkolehmuskorpi@gmail.com>
2019-07-28 13:35:03 +0300

    Refactor blogs service

--- osa5/bloglist-frontend/src/services/blogs.js
+++ osa5/bloglist-frontend/src/services/blogs.js
@@ -2,43 +2,47 @@ import axios from 'axios';
 
 const baseUrl = '/api/blogs';
 
+const headers = token => {
+  return {
+    headers: {
+      Authorization: `bearer ${token}`,
+    },
+  };
+};
+
+// getAll
 const getAll = async () => {
   const response = await axios.get(baseUrl);
 
   return response.data;
 };
 
+// createNew
 const createNew = async (blog, token) => {
-  const response = await axios.post(baseUrl, blog, {
-    headers: {
-      Authorization: `bearer ${token}`,
-    },
-  });
+  const response = await axios.post(baseUrl, blog, headers(token));
 
   return response.data;
 };
 
+// update
 const update = async (blog, updatedFields, token) => {
   const blogObj = {
     ...blog,
     ...updatedFields,
   };
 
-  const response = await axios.put(`${baseUrl}/${blog.id}`, blogObj, {
-    headers: {
-      Authorization: `bearer ${token}`,
-    },
-  });
+  const response = await axios.put(
+    `${baseUrl}/${blog.id}`,
+    blogObj,
+    headers(token)
+  );
 
   return response.data;
 };
 
+// remove
 const remove = async (blog, token) => {
-  const response = await axios.delete(`${baseUrl}/${blog.id}`, {
-    headers: {
-      Authorization: `bearer ${token}`,
-    },
-  });
+  const response = await axios.delete(`${baseUrl}/${blog.id}`, headers(token));
 
   return response.data;
 };
