baca08e9bb31a3aba309b520a3e8f00a557bbdaa
VeikkoLehmuskorpi <veikko.lehmuskorpi@student.lamk.fi>
2019-07-29 12:51:12 +0300

    Sort anecdotes by votes

--- osa6/anecdotes/src/App.js
+++ osa6/anecdotes/src/App.js
@@ -33,13 +33,15 @@ const AnecdoteForm = ({ store }) => {
 const AnecdoteList = ({ store }) => {
   const anecdotes = store.getState();
 
+  const sortedAnecdotes = anecdotes.sort((a, b) => b.votes - a.votes);
+
   const vote = id => {
     console.log('vote', id);
 
     store.dispatch(voteAnecdote(id));
   };
 
-  return anecdotes.map(anecdote => (
+  return sortedAnecdotes.map(anecdote => (
     <Anecdote key={anecdote.id} anecdote={anecdote} handleClick={() => vote(anecdote.id)} />
   ));
 };
