a5b6dd4b1563a50693d4d81647627e855f2ad9d0
VeikkoLehmuskorpi <veikko.lehmuskorpi@student.lamk.fi>
2019-07-29 14:21:06 +0300

    Add Notification component to App

--- osa6/anecdotes/src/App.js
+++ osa6/anecdotes/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Notification from './components/Notification';
 import AnecdoteForm from './components/AnecdoteForm';
 import AnecdoteList from './components/AnecdoteList';
 
 const App = ({ store }) => {
   return (
     <div>
+      <Notification store={store} />
       <h2>Anecdotes</h2>
       <AnecdoteList store={store} />
       <h2>create new</h2>
