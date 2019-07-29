b2b049520b91d5ed5310db23931147ab60bf06fd
VeikkoLehmuskorpi <veikko.lehmuskorpi@student.lamk.fi>
2019-07-29 14:20:16 +0300

    Combine anecdote and notification reducers

--- osa6/anecdotes/src/index.js
+++ osa6/anecdotes/src/index.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 import App from './App';
-import reducer from './reducers/anecdoteReducer';
+import anecdoteReducer from './reducers/anecdoteReducer';
+import notificationReducer from './reducers/notificationReducer';
+
+const reducer = combineReducers({
+  anecdotes: anecdoteReducer,
+  notification: notificationReducer,
+});
 
 const store = createStore(reducer);
+
+store.dispatch({
+  type: 'SET_NOTIFICATION',
+  data: {
+    message: 'Lorem ipsum dolor sit amet',
+    type: 'success',
+  },
+});
+
+console.log(store.getState());
 
 const render = () => {
   ReactDOM.render(<App store={store} />, document.getElementById('root'));
 };
 
 render();
-store.subscribe(render);
+store.subscribe(() => {
+  console.log(store.getState());
+  render();
+});
