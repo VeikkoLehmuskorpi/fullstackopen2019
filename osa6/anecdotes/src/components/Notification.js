d528052bb3814ddf3459126ea1368b4841db2bba
VeikkoLehmuskorpi <veikko.lehmuskorpi@student.lamk.fi>
2019-07-29 14:20:56 +0300

    Display Notification component with a message from state

--- osa6/anecdotes/src/components/Notification.js
+++ osa6/anecdotes/src/components/Notification.js
@@ -1,12 +1,30 @@
 import React from 'react';
 
-const Notification = () => {
+const Notification = ({ store }) => {
+  const { notification } = store.getState();
+
+  const notificationColor = () => {
+    switch (notification.type) {
+      case 'success':
+        return 'green';
+      case 'warning':
+        return 'orange';
+      case 'danger':
+        return 'red';
+      default:
+        return 'black';
+    }
+  };
+
   const style = {
+    color: notificationColor(),
     border: 'solid',
+    borderColor: notificationColor(),
     padding: 10,
     borderWidth: 1,
   };
-  return <div style={style}>render here notification...</div>;
+
+  return notification.message ? <div style={style}>{notification.message}</div> : null;
 };
 
 export default Notification;
