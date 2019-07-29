0e144a4ee49bd5094ba178d7807720b0ec42fec5
VeikkoLehmuskorpi <veikko.lehmuskorpi@student.lamk.fi>
2019-07-29 14:36:13 +0300

    Display and hide a notification after voting on an anecdote

--- osa6/anecdotes/src/components/AnecdoteList.js
+++ osa6/anecdotes/src/components/AnecdoteList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { voteAnecdote } from '../reducers/anecdoteReducer';
+import { notificationSet, notificationRemove } from '../reducers/notificationReducer';
 import Anecdote from './Anecdote';
 
 const AnecdoteList = ({ store }) => {
@@ -7,14 +8,22 @@ const AnecdoteList = ({ store }) => {
 
   const sortedAnecdotes = anecdotes.sort((a, b) => b.votes - a.votes);
 
-  const vote = id => {
+  const vote = ({ content, id }) => {
     console.log('vote', id);
 
+    // Send vote action
     store.dispatch(voteAnecdote(id));
+    // Send notification action
+    store.dispatch(notificationSet({ message: `You voted "${content}"`, type: 'success' }));
+
+    // Remove notification after 5 seconds
+    setTimeout(() => {
+      store.dispatch(notificationRemove());
+    }, 5000);
   };
 
   return sortedAnecdotes.map(anecdote => (
-    <Anecdote key={anecdote.id} anecdote={anecdote} handleClick={() => vote(anecdote.id)} />
+    <Anecdote key={anecdote.id} anecdote={anecdote} handleClick={() => vote(anecdote)} />
   ));
 };
 
