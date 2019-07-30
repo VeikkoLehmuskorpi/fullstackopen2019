057cc44112282d03d71c126d8b65432ffbaeadf6
VeikkoLehmuskorpi <veikko.lehmuskorpi@student.lamk.fi>
2019-07-30 10:15:02 +0300

    Create init action for anecdotes

--- osa6/anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/anecdotes/src/reducers/anecdoteReducer.js
@@ -1,23 +1,4 @@
-import uuid from 'uuid/v4';
-
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-];
-
-const asObject = anecdote => {
-  return {
-    content: anecdote,
-    id: uuid(),
-    votes: 0,
-  };
-};
-
-const initialState = anecdotesAtStart.map(asObject);
+const initialState = [];
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
@@ -34,6 +15,8 @@ const reducer = (state = initialState, action) => {
       return newState;
     case 'ADD_ANECDOTE':
       return [...state, action.data];
+    case 'INIT_ANECDOTES':
+      return action.data;
     default:
       return state;
   }
@@ -50,13 +33,16 @@ export const voteAnecdote = id => {
   };
 };
 
-export const createAnecdote = content => {
+export const createAnecdote = data => {
   return {
     type: 'ADD_ANECDOTE',
-    data: {
-      content,
-      id: uuid(),
-      votes: 0,
-    },
+    data,
+  };
+};
+
+export const initializeAnecdotes = data => {
+  return {
+    type: 'INIT_ANECDOTES',
+    data,
   };
 };
