822c0dab6caba68bb90e9502dfefbae8f6e25e5c
VeikkoLehmuskorpi <veikkolehmuskorpi@gmail.com>
2019-07-31 19:22:58 +0300

    Include LoginForm inside navbar and style menu

--- osa7/bloglist/bloglist-frontend/src/components/Menu.js
+++ osa7/bloglist/bloglist-frontend/src/components/Menu.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import LoginForm from './LoginForm';
 
 const Menu = () => {
   return (
-    <div>
-      <Link to='/'>Blogs</Link>
-      <Link to='/users'>Users</Link>
+    <div
+      style={{ background: '#f4f4f4', padding: '.5rem', marginBottom: '1rem' }}
+    >
+      <Link style={{ marginRight: '1rem' }} to='/'>
+        Blogs
+      </Link>
+      <Link style={{ marginRight: '1rem' }} to='/users'>
+        Users
+      </Link>
+      <LoginForm style={{ marginRight: '1rem' }} />
     </div>
   );
 };
