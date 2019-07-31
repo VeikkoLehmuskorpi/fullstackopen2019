ffe7f70b39c6acab6c93ec80025b12ccdff6cd9e
VeikkoLehmuskorpi <veikkolehmuskorpi@gmail.com>
2019-07-31 18:28:31 +0300

    Create a table of users

--- osa7/bloglist/bloglist-frontend/src/components/UserList.js
+++ osa7/bloglist/bloglist-frontend/src/components/UserList.js
@@ -1,11 +1,52 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const UserList = () => {
+const UserList = ({ blogs }) => {
+  const users = blogs.map(blog => blog.user);
+
+  const uniqueUserIds = [...new Set(users.map(user => user.id))];
+
+  const uniqueUsers = uniqueUserIds
+    .map(id => {
+      return users.find(user => user.id === id);
+    })
+    .map(user => ({
+      ...user,
+      blogs: users.filter(u => u.id === user.id).length,
+    }));
+
   return (
     <div>
       <h2>Users</h2>
+      <table>
+        <thead>
+          <tr>
+            <td></td>
+            <td>
+              <strong>Blogs</strong>
+            </td>
+          </tr>
+        </thead>
+        <tbody>
+          {uniqueUsers.map(user => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.blogs}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
 
-export default UserList;
+const mapStateToProps = state => {
+  return {
+    blogs: state.blogs,
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  null
+)(UserList);
