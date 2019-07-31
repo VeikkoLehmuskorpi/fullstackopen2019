da9eaa6562cdafd77c680f42ce4a2f3c4a60aaaf
VeikkoLehmuskorpi <veikko.lehmuskorpi@student.lamk.fi>
2019-07-31 15:48:06 +0300

    Implement react router to display UserList

--- osa7/bloglist/bloglist-frontend/src/App.js
+++ osa7/bloglist/bloglist-frontend/src/App.js
@@ -2,11 +2,13 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { initBlogs } from './reducers/blogReducer';
 import { initUser } from './reducers/userReducer';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import LoginForm from './components/LoginForm';
 import BlogList from './components/BlogList';
 import BlogForm from './components/BlogForm';
 import Notification from './components/Notification';
 import Togglable from './components/Togglable';
+import UserList from './components/UserList';
 
 const App = ({ user, initUser, initBlogs }) => {
   useEffect(() => {
@@ -28,13 +30,26 @@ const App = ({ user, initUser, initBlogs }) => {
 
       <LoginForm />
 
-      {user && (
-        <Togglable showLabel='New note' ref={blogFormRef}>
-          <BlogForm blogFormRef={blogFormRef} />
-        </Togglable>
-      )}
+      <Router>
+        <>
+          <Route
+            exact
+            path='/'
+            render={() => (
+              <>
+                {user && (
+                  <Togglable showLabel='New note' ref={blogFormRef}>
+                    <BlogForm blogFormRef={blogFormRef} />
+                  </Togglable>
+                )}
 
-      <BlogList />
+                <BlogList />
+              </>
+            )}
+          ></Route>
+          <Route path='/users' render={() => <UserList></UserList>}></Route>
+        </>
+      </Router>
     </>
   );
 };
