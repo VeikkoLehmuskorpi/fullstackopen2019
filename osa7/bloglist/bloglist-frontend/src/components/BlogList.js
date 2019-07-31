818c3f5e94366a37c6dc81664dce17090792ad25
VeikkoLehmuskorpi <veikkolehmuskorpi@gmail.com>
2019-07-31 18:55:00 +0300

    Make each blog in BlogList a link

--- osa7/bloglist/bloglist-frontend/src/components/BlogList.js
+++ osa7/bloglist/bloglist-frontend/src/components/BlogList.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import Blog from './Blog';
 
 const BlogList = ({ blogs, user }) => {
   if (user === null) return null;
 
-  return blogs.map(blog => <Blog key={blog.id} blog={blog} user={user} />);
+  return blogs.map(blog => (
+    <Link key={blog.id} to={`/blogs/${blog.id}`}>
+      <Blog blog={blog} user={user} />
+    </Link>
+  ));
 };
 
 const sortedBlogs = blogs => blogs.sort((a, b) => b.likes - a.likes);
