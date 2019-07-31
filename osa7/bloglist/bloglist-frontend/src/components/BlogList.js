9581df127da3864ec578bb6ef5cb4eb1decf2904
VeikkoLehmuskorpi <veikko.lehmuskorpi@student.lamk.fi>
2019-07-31 15:29:04 +0300

    Make BlogList purely a presentational component

--- osa7/bloglist/bloglist-frontend/src/components/BlogList.js
+++ osa7/bloglist/bloglist-frontend/src/components/BlogList.js
@@ -5,14 +5,14 @@ import Blog from './Blog';
 const BlogList = ({ blogs, user }) => {
   if (user === null) return null;
 
-  return blogs
-    .sort((a, b) => b.likes - a.likes)
-    .map(blog => <Blog key={blog.id} blog={blog} user={user} />);
+  return blogs.map(blog => <Blog key={blog.id} blog={blog} user={user} />);
 };
+
+const sortedBlogs = blogs => blogs.sort((a, b) => b.likes - a.likes);
 
 const mapStateToProps = state => {
   return {
-    blogs: state.blogs,
+    blogs: sortedBlogs(state.blogs),
     user: state.user,
   };
 };
