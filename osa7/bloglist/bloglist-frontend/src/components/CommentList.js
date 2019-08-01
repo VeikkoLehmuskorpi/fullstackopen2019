aa4927774e38566da8e310552753ed465c5c7e5b
VeikkoLehmuskorpi <veikkolehmuskorpi@gmail.com>
2019-08-01 18:56:21 +0300

    Render CommentForm but not list if no comments

--- osa7/bloglist/bloglist-frontend/src/components/CommentList.js
+++ osa7/bloglist/bloglist-frontend/src/components/CommentList.js
@@ -2,19 +2,19 @@ import React from 'react';
 import CommentForm from './CommentForm';
 
 const CommentList = ({ blog, user }) => {
-  if (blog.comments.length === 0) return null;
-
   return (
     <div>
       <h2>Comments</h2>
 
       <CommentForm blog={blog} user={user}></CommentForm>
 
-      <ul>
-        {blog.comments.map(comment => (
-          <li key={comment.timestamp}>{comment.comment}</li>
-        ))}
-      </ul>
+      {blog.comments.length > 0 && (
+        <ul>
+          {blog.comments.map(comment => (
+            <li key={comment.timestamp}>{comment.comment}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
