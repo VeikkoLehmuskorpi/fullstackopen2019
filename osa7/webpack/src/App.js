6d2260711f949426f499c70099add7ca788e7a43
VeikkoLehmuskorpi <veikko.lehmuskorpi@student.lamk.fi>
2019-08-02 13:29:51 +0300

    Add json-server

--- osa7/webpack/src/App.js
+++ osa7/webpack/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const App = () => {
   const [counter, setCounter] = useState(0);
@@ -9,10 +10,17 @@ const App = () => {
     setValues(values.concat(counter));
   };
 
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/notes').then(response => setNotes(response.data));
+  }, []);
+
   return (
     <div>
       <h1>{counter}</h1>
       <button onClick={handleClick}>+</button>
+      <p>{notes.length} notes on the server</p>
     </div>
   );
 };
