9c66a57abf38478a6a9586e4864e936ab60f96c8
VeikkoLehmuskorpi <veikkolehmuskorpi@gmail.com>
2019-08-03 13:03:30 +0300

    Clear test db before cypress tests

--- osa7/bloglist/bloglist-frontend/cypress/integration/blog_app_spec.js
+++ osa7/bloglist/bloglist-frontend/cypress/integration/blog_app_spec.js
@@ -1,7 +1,21 @@
 /* eslint-disable */
 describe('Blog app', function() {
   beforeEach(function() {
+    cy.request('POST', 'http://localhost:3002/api/testing/reset');
+    const user = {
+      name: 'Testi Testaaja',
+      username: 'ttestaaja',
+      password: 'admin',
+    };
+    cy.request('POST', 'http://localhost:3002/api/users/', user);
     cy.visit('http://localhost:3000');
+  });
+
+  it('testing db has been successfully cleared', function() {
+    cy.get('#login-username-input').type('vlehmuskorpi');
+    cy.get('#login-password-input').type('salasana');
+    cy.get('#login-submit').click();
+    cy.contains('Invalid username or password.');
   });
 
   it('can be opened', function() {
@@ -11,17 +25,17 @@ describe('Blog app', function() {
 
   describe('login', function() {
     it('doesnt work with invalid credentials', function() {
-      cy.get('#login-username-input').type('wrongusername');
-      cy.get('#login-password-input').type('wrongpassword');
+      cy.get('#login-username-input').type('vlehmuskorpi');
+      cy.get('#login-password-input').type('salasana');
       cy.get('#login-submit').click();
       cy.contains('Invalid username or password.');
     });
 
-    it('works with correct credentials', function() {
-      cy.get('#login-username-input').type('vlehmuskorpi');
-      cy.get('#login-password-input').type('salasana');
+    it('works with tester credentials', function() {
+      cy.get('#login-username-input').type('ttestaaja');
+      cy.get('#login-password-input').type('admin');
       cy.get('#login-submit').click();
-      cy.contains('Logged in as vlehmuskorpi');
+      cy.contains('Logged in as ttestaaja');
     });
   });
 });
